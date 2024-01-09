481850fb3a27fe5f9a6ccbd2f5244095dc895657
Vivek Maddeshiya <vivekmaddheshiya205@gmail.com>
2024-01-09 23:34:00 +0530

    added services section and changed swiper timing

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var swiper = new Swiper(".product-row", {
     loop:true,
     centeredSlides:true,
     autoplay:{
-        delay:9500,
+        delay:3000,
         disableOnInteraction:false,
     },
     pagination: {
@@ -41,7 +41,7 @@ var swiper = new Swiper(".blogs-row", {
     loop:true,
     centeredSlides:true,
     autoplay:{
-        delay:9500,
+        delay:3000,
         disableOnInteraction:false,
     },
     pagination: {
@@ -70,7 +70,7 @@ var swiper = new Swiper(".blogs-row", {
     loop:true,
     centeredSlides:true,
     autoplay:{
-        delay:9500,
+        delay:3000,
         disableOnInteraction:false,
     },
     pagination: {
